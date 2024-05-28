048b499370acaa70fcf350e22482718fdf150ab5
Miyako <miyako@talltalesmarketing.com>
2024-05-28 10:49:31 +0800

    first commit

---
+++ cypress/e2e/signIn.cy.js
@@ -0,0 +1,12 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // Fail the test
+  throw err;
+  // Return false to prevent the error from failing the test in an unexpected way
+  return false;
+});
+
+it('Sign in',()=>{
+
+cy.visit('https://www.my.jufsolution1.com')
+
+})
